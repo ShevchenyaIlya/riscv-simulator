ff882344f0efd9df589abbed5596c40280a9b83b
ShevchenyaIlya <shevchenya.i@gmail.com>
2020-04-29 12:40:37 +0300

    Change main structure for working with new mamory.

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,30 @@
 #include "Memory.h"
 #include "BaseTypes.h"
 
-#include "test_decoder.h"
-#include "test_executor.h"
-
 #include <optional>
+
+
+// First task. Instruction per tact: 0.007611794
 
 int main()
 {
-    Memory mem;
-    mem.LoadElf("../program");
-    Cpu cpu{mem};
+    MemoryStorage mem ;
+    //mem.LoadElf("../programs/build/assembly/dump/add.riscv");
+    mem.LoadElf("program");
+    // TODO: Change this stupid names
+    UncachedMem memory = UncachedMem (mem);
+    //Word value = ToLineAddr(1);
+    //Word offset = ToLineOffset(600);
+    CachedMem mema = CachedMem(memory);
+    std::unique_ptr<CachedMem> memModelPtr( new CachedMem(memory));
+    Cpu cpu{*memModelPtr};
     cpu.Reset(0x200);
-
-    bool test_result = test_decode();
-    bool test_execute = test_executor();
 
     int32_t print_int = 0;
     while (true)
     {
-        cpu.ProcessInstruction();
+        cpu.Clock();
+        memModelPtr->Clock();
         std::optional<CpuToHostData> msg = cpu.GetMessage();
         if (!msg)
             continue;
